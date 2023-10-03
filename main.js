10e9fa7cda7e3cdfc71875b59d5b447582e7ee67
JulieBourelDrapier <juliebourel@hotmail.com>
2023-10-03 02:04:35 +0200

    config files (html, css, js) + 1st animation

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+  strings: ["Développeuse front-end", "Intégratrice web", "Curieuse et passionnée"],
+  typeSpeed: 100,
+  backSpeed: 200,
+  backDelay: 1000,
+  loop: true
+})
