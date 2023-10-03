bc1a45e7a39f1cb6be46e3f32973ebd9d87904ee
JulieBourelDrapier <juliebourel@hotmail.com>
2023-10-03 16:59:02 +0200

    add ''about section''

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-  strings: ["développeuse front", "intégratrice web", "curieuse et passionnée"],
+  strings: ["développeuse front", "intégratrice web", "curieuse et motivée"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
