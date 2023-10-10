07d9ef676f1cb31d8848d1581bc6aa82ccad89bf
JulieBourelDrapier <juliebourel@hotmail.com>
2023-10-10 14:24:15 +0200

    modification of image w1500 + webp

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-// var typed = new Typed(".text",{
-//   strings: [" ", "développeuse front-end", "curieuse et motivée", "disponible dès lundi !"],
-//   typeSpeed: 50,
-//   backSpeed: 50,
-//   backDelay: 1800,
-//   loop: true
-// })
+// presentation animation
+var typed = new Typed(".text",{
+  strings: [" ", "développeuse front-end", "curieuse et motivée", "disponible dès lundi !"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1800,
+  loop: true
+})
 
-// // carousel
-// const slides = document.querySelector(".slides");
-// const slidesCount = slides.childElementCount;
-// const maxLeft = (slidesCount - 1) * 100 * -1;
-
-// let current = 0;
-
-// function changeSlide(next = true) {
-//   if (next) {
-//     current += current > maxLeft ? -100 : current * -1;
-//   } else {
-//     current = current < 0 ? current + 100 : maxLeft;
-//   }
-
-//   slides.style.left = current + "%";
-// }
-
-// // Controls
-// document.querySelector(".next-slide").addEventListener("click", function() {
-//   changeSlide();
-//   restart();
-// });
-
-// document.querySelector(".prev-slide").addEventListener("click", function() {
-//   changeSlide(false);
-//   restart();
-// });
-
-
-// galerie
+// gallery
 var splide = new Splide( '#main-carousel', {
   pagination: false,
 } );
